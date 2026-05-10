1dacf8587f0f002330b76297d3b1fe187c37b53f
Niklas Lundberg <info@nlundberg.se>
2026-05-10 19:41:21 +0200

    Restore TestAppBuilder.

--- src/Arbor.HttpClient.Desktop.E2E.Tests/TestAppBuilder.cs
+++ src/Arbor.HttpClient.Desktop.E2E.Tests/TestAppBuilder.cs
@@ -10,8 +10,7 @@ public static class TestAppBuilder
 {
     public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
         .UseSkia()
-        .UseHeadless(new AvaloniaHeadlessPlatformOptions
-        {
-            UseHeadlessDrawing = false
-        });
+        .UseHeadless(new AvaloniaHeadlessPlatformOptions { UseHeadlessDrawing = false })
+        .WithInterFont()
+        .LogToTrace();
 }
